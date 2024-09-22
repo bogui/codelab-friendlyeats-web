e57230366c801178bb108da0e2b55930a01ab16d
Bogomil Krustev <bogomil@krustev.dev>
2024-09-22 15:03:58 +0300

    firebase-config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,23 +2,40 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-bogui","appId":"1:464489980706:web:061dcd29bbfe031f4dbb7b","storageBucket":"friendlychat-bogui.appspot.com","locationId":"europe-west","apiKey":"AIzaSyA451mEM-92Zv8lytsc8jmawkO5UfiHGaE","authDomain":"friendlychat-bogui.firebaseapp.com","messagingSenderId":"464489980706","measurementId":"G-KYJB60DVQR"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    // importProvidersFrom(
+    //   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    //   provideFirestore(() => getFirestore()),
+    //   provideAuth(() => getAuth()),
+    //   provideFunctions(() => getFunctions()),
+    //   provideStorage(() => getStorage()),
+    //   provideMessaging(() => getMessaging())
+    // ),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,14 @@
 export const environment = {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyA451mEM-92Zv8lytsc8jmawkO5UfiHGaE',
+    authDomain: 'friendlychat-bogui.firebaseapp.com',
+    projectId: 'friendlychat-bogui',
+    storageBucket: 'friendlychat-bogui.appspot.com',
+    messagingSenderId: '464489980706',
+    appId: '1:464489980706:web:061dcd29bbfe031f4dbb7b',
+    measurementId: 'G-KYJB60DVQR',
+  },
   firebase: {
     apiKey: 'API_KEY',
     authDomain: 'PROJECT_ID.firebaseapp.com',
